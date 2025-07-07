4d7cd03358bff26948af4a18de75bb611e466ec5
Stella Uzunova <stellauznv@gmail.com>
2025-07-07 16:52:06 +0300

    Add project files.

---
+++ BookShopApplication.Web.ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookShopApplication.Web.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
