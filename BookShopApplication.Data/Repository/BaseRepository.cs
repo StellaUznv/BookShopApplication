858ae466c0382757c9d386ed7faad335f4d07fab
Stella Uzunova <stellauznv@gmail.com>
2025-07-15 15:08:26 +0300

    BaseRepository abstract class created. Method implementation started.

---
+++ BookShopApplication.Data/Repository/BaseRepository.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using BookShopApplication.Data.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace BookShopApplication.Data.Repository
+{
+    public abstract class BaseRepository<TEntity, TKey> : IRepository<TEntity, TKey> 
+        , IRepositoryAsync<TEntity,TKey>
+    where TEntity : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<TEntity> dbSet;
+
+        protected BaseRepository(ApplicationDbContext context)
+        {
+            this._context = context;
+            this.dbSet = this._context.Set<TEntity>();
+        }
+
+        public TEntity GetById(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity FirstOrDefault(Func<TEntity, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<TEntity> GetAllAttached()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(TEntity[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(TKey id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> FirstOrDefaultAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddAsync(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddRangeAsync(TEntity[] items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
