a4e8a1485ec6c0411fc71a8fd5b1551cca38ae51
Stella Uzunova <stellauznv@gmail.com>
2025-07-15 14:54:01 +0300

    IRepository interface added.

---
+++ BookShopApplication.Data/Repository/Contracts/IRepositoryInterface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopApplication.Data.Repository.Contracts
+{
+    public interface IRepositoryInterface<TEntity, TKey>
+    {
+        TEntity GetById(TKey id);
+
+        TEntity FirstOrDefault(Func<TEntity, bool> predicate);
+        IEnumerable<TEntity> GetAll();
+
+        IQueryable<TEntity> GetAllAttached();
+
+        void Add(TEntity item);
+
+        void AddRange(TEntity[] items);
+
+        bool Delete(TEntity entity);
+        bool Update(TEntity item);
+    }
+}
