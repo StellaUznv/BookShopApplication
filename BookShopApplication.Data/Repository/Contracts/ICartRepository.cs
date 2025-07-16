0cb9dfbf89738247b17d33ebbdf050b5df6f6337
Stella Uzunova <stellauznv@gmail.com>
2025-07-16 15:16:34 +0300

    ICartRepository and CartRepository added. BookService updated to work only with repositories and GetCartItems method implemented via CartRepository.

---
+++ BookShopApplication.Data/Repository/Contracts/ICartRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookShopApplication.Data.Models;
+
+namespace BookShopApplication.Data.Repository.Contracts
+{
+    public interface ICartRepository : IRepository<CartItem, Guid> , IAsyncRepository<CartItem,Guid>
+    {
+       public Task<List<Guid>> GetCartItemsIdsAsNoTrackingAsync(Guid userId);
+    }
+}
