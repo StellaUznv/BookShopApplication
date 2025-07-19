30ac225e1ea8e6a24fe6a73319f75f7f4b3be24b
Stella Uzunova <stellauznv@gmail.com>
2025-07-19 14:58:08 +0300

    Shop Index action added to the controller and working with a view. Needs enhancement.

---
+++ BookShopApplication.Web/Controllers/ShopController.cs
@@ -0,0 +1,34 @@
+﻿using BookShopApplication.Data.Models;
+using BookShopApplication.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BookShopApplication.Web.Controllers
+{
+    public class ShopController : Controller
+    {
+        private readonly IShopService _shopService;
+
+        public ShopController(IShopService service)
+        {
+            this._shopService = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            Guid? userId = null;
+
+            if (User.Identity != null && User.Identity.IsAuthenticated)
+            {
+                var idValue = User.FindFirstValue(ClaimTypes.NameIdentifier);
+                if (!string.IsNullOrEmpty(idValue))
+                {
+                    userId = Guid.Parse(idValue);
+                }
+            }
+
+            var models = await _shopService.DisplayAllShopsAsync(userId);
+            return View(models);
+        }
+    }
+}
