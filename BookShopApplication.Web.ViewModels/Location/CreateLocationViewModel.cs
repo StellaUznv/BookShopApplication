5305c2bc9656718682cace2edd668fad5392a56f
Stella Uzunova <stellauznv@gmail.com>
2025-07-19 16:08:35 +0300

    Create Location ViewModel and LocationServoce added.

---
+++ BookShopApplication.Web.ViewModels/Location/CreateLocationViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopApplication.Web.ViewModels.Location
+{
+    public class CreateLocationViewModel
+    {
+        [Required] 
+        public string CountryName { get; set; } = null!;
+
+        [Required]
+        public string CityName { get; set; } = null!;
+
+        [Required]
+        public string Address { get; set; } = null!;
+
+        [Required]
+        public string ZipCode { get; set; } = null!;
+
+
+    }
+}
