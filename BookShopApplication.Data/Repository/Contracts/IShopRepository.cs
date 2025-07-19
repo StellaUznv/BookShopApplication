e7577e30bb4faf77524ffe45fed472ebd326d87e
Stella Uzunova <stellauznv@gmail.com>
2025-07-19 14:27:12 +0300

    ShopRepository created.

---
+++ BookShopApplication.Data/Repository/Contracts/IShopRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookShopApplication.Data.Models;
+
+namespace BookShopApplication.Data.Repository.Contracts
+{
+    public interface IShopRepository : IRepository<Shop, Guid> , IAsyncRepository<Shop,Guid>
+    {
+    }
+}
---
+++ BookShopApplication.Data/Repository/ShopRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookShopApplication.Data.Models;
+using BookShopApplication.Data.Repository.Contracts;
+
+namespace BookShopApplication.Data.Repository
+{
+    public class ShopRepository : BaseRepository<Shop, Guid>, IShopRepository
+    {
+        public ShopRepository(ApplicationDbContext context) 
+            : base(context)
+        {
+        }
+    }
+}
