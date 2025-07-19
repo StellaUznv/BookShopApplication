1d3b515a202e068ef99bbd6bf4ee33cff0662819
Stella Uzunova <stellauznv@gmail.com>
2025-07-19 14:40:42 +0300

    DisplayAllShopsAsync method added to ShopService.

--- BookShopApplication.Services/Contracts/IShopService.cs
+++ BookShopApplication.Services/Contracts/IShopService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookShopApplication.Web.ViewModels.Shop;
 
 namespace BookShopApplication.Services.Contracts
 {
     public interface IShopService
     {
+        public Task<IEnumerable<ShopViewModel>> DisplayAllShopsAsync(Guid? userId);
     }
 }
--- BookShopApplication.Services/ShopService.cs
+++ BookShopApplication.Services/ShopService.cs
@@ -3,11 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookShopApplication.Data.Repository.Contracts;
 using BookShopApplication.Services.Contracts;
+using BookShopApplication.Web.ViewModels.Shop;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookShopApplication.Services
 {
     public class ShopService : IShopService
     {
+        private readonly IShopRepository _shopRepository;
+
+        public ShopService(IShopRepository shopRepository)
+        {
+            _shopRepository = shopRepository;
+        }
+
+
+        public async Task<IEnumerable<ShopViewModel>> DisplayAllShopsAsync(Guid? userId)
+        {
+            var shopsModels = await _shopRepository.GetAllAttached().Select(s => new ShopViewModel
+            {
+                Id = s.Id,
+                Description = s.Description,
+                Name = s.Name,
+                Latitude = s.Latitude,
+                Longitude = s.Longitude,
+                LocationAddress = s.Location.Address,
+                LocationCity = s.Location.CityName
+            }).AsNoTracking()
+                .ToListAsync();
+
+            return shopsModels;
+        }
     }
 }
