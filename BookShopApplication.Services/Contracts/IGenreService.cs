d4e9db0dfd1df577d81318965fd6ff93d041e2e2
Stella Uzunova <stellauznv@gmail.com>
2025-07-22 18:31:48 +0300

    GenreViewModel and GetGenreListAsync() method added to Genre service.

--- BookShopApplication.Services/Contracts/IGenreService.cs
+++ BookShopApplication.Services/Contracts/IGenreService.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookShopApplication.Web.ViewModels.Genre;
 
 namespace BookShopApplication.Services.Contracts
 {
     public interface IGenreService
     {
+        public Task<IEnumerable<GenreViewModel>> GetGenreListAsync();
     }
 }
--- BookShopApplication.Services/GenreService.cs
+++ BookShopApplication.Services/GenreService.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using BookShopApplication.Data.Repository.Contracts;
 using BookShopApplication.Services.Contracts;
+using BookShopApplication.Web.ViewModels.Genre;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookShopApplication.Services
 {
@@ -15,6 +17,18 @@ namespace BookShopApplication.Services
         public GenreService(IGenreRepository genreRepository)
         {
             _genreRepository = genreRepository;
+        }
+
+        public async Task<IEnumerable<GenreViewModel>> GetGenreListAsync()
+        {
+            var genres = await _genreRepository.GetAllAttached().Select(g => new GenreViewModel
+            {
+                Id = g.Id,
+                Name = g.Name,
+                Description = g.Description,
+            }).ToListAsync();
+
+            return genres;
         }
     }
 }
