3f3fe0037562fade41d2399af45261d676de5c12
Stella Uzunova <stellauznv@gmail.com>
2025-07-22 18:51:15 +0300

    Book Create action added. Need to figure out the genre creation and the picture upload to be saved in the filder.

--- BookShopApplication.Web/Areas/Manager/Controllers/BookController.cs
+++ BookShopApplication.Web/Areas/Manager/Controllers/BookController.cs
@@ -1,6 +1,8 @@
 ﻿using BookShopApplication.Services.Contracts;
+using BookShopApplication.Web.ViewModels.Book;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookShopApplication.Web.Areas.Manager.Controllers
 {
@@ -9,11 +11,28 @@ namespace BookShopApplication.Web.Areas.Manager.Controllers
     public class BookController : Controller
     {
         private readonly IBookService _bookService;
+        private readonly IGenreService _genreService;
 
-        public BookController(IBookService bookService)
+        public BookController(IBookService bookService, IGenreService genreService)
         {
             _bookService = bookService;
+            _genreService = genreService;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var genresModel = await _genreService.GetGenreListAsync();
+
+            var model = new CreateBookViewModel
+            {
+                Genres = genresModel.Select(g=> new SelectListItem
+                {
+                    Text = g.Name,
+                    Value = g.Id.ToString()
+                }).ToList()
+            };
+            return View(model);
+        }
     }
 }
