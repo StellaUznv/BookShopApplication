6cdc8fd51e810ff6094a4069693b54ea8be8896c
Stella Uzunova <stellauznv@gmail.com>
2025-07-22 18:17:52 +0300

    CreateBookViewModel added.

---
+++ BookShopApplication.Web.ViewModels/Book/CreateBookViewModel.cs
@@ -0,0 +1,48 @@
+﻿using BookShopApplication.Data.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopApplication.Web.ViewModels.Book
+{
+    public class CreateBookViewModel
+    {
+        [Required]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required]
+        public string Author { get; set; } = null!;
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int PagesNumber { get; set; }
+
+        public string? ImagePath { get; set; }
+
+        [Required]
+        public bool IsDeleted { get; set; } = false;
+
+        [Required(ErrorMessage = "Please select a genre or enter a new one.")]
+        public Guid? GenreId { get; set; }  // Nullable so user can choose new genre instead
+
+        [Required]
+        public Guid ShopId { get; set; }
+
+        // List of genres for the dropdown
+        public IEnumerable<SelectListItem>? Genres { get; set; }
+    }
+
+}
---
+++ BookShopApplication.Web/Areas/Manager/Controllers/BookController.cs
@@ -0,0 +1,19 @@
+﻿using BookShopApplication.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopApplication.Web.Areas.Manager.Controllers
+{
+    [Area("Manager")]
+    [Authorize(Roles = "Manager")]
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+    }
+}
