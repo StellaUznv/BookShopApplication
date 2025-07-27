1774dd9ba4f0b592d5f6940768e4c8200cb4860c
Stella Uzunova <stellauznv@gmail.com>
2025-07-27 17:25:23 +0300

    Now can acces all books from admin dashboard.

---
+++ BookShopApplication.Web/Areas/Admin/Controllers/BookController.cs
@@ -0,0 +1,27 @@
+﻿using BookShopApplication.Data.Models;
+using BookShopApplication.Data.Repository.Contracts;
+using BookShopApplication.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BookShopApplication.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class BookController : Controller
+    {
+        private readonly IBookService _bookService;
+
+        public BookController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var models = await _bookService.DisplayAllBooksAsync();
+            return View(models);
+        }
+    }
+}
