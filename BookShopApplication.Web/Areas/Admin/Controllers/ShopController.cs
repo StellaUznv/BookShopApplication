381d3824f417694f6d55678e44f0f80f78baff5a
Stella Uzunova <stellauznv@gmail.com>
2025-07-27 18:29:30 +0300

    Shops now visible trough admin dashboard.

---
+++ BookShopApplication.Web/Areas/Admin/Controllers/ShopController.cs
@@ -0,0 +1,24 @@
+﻿using BookShopApplication.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopApplication.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ShopController : Controller
+    {
+        private readonly IShopService _shopService;
+
+        public ShopController(IShopService shopService)
+        {
+            _shopService = shopService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var models = await _shopService.GetAllShopsWithBooksAsync();
+            return View(models);
+        }
+    }
+}
