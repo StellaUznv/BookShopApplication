35dbb0c791409d6770b7a1e3fb7f3455b19a3acd
Stella Uzunova <stellauznv@gmail.com>
2025-07-27 19:20:12 +0300

    Role edit and delete available, Role CRUD done.

---
+++ BookShopApplication.Web.ViewModels/Role/RoleEditViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopApplication.Web.ViewModels.Role
+{
+    public class RoleEditViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "Role name is required.")]
+        [StringLength(256)]
+        public string Name { get; set; } = null!;
+
+        public List<UserRoleAssignmentViewModel> Users { get; set; } = new();
+    }
+}
