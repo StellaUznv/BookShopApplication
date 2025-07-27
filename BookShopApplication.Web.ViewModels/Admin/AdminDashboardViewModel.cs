4d693d7ee4b15dcacba60c57295f096651b6771f
Stella Uzunova <stellauznv@gmail.com>
2025-07-27 17:24:58 +0300

    Admin Controller with index action added to pass to the view the AdminDashboardViewModel and a view for the dashboard creaded.

---
+++ BookShopApplication.Web.ViewModels/Admin/AdminDashboardViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopApplication.Web.ViewModels.Admin
+{
+    public class AdminDashboardViewModel
+    {
+        public int TotalBooks { get; set; }
+        public int TotalShops { get; set; }
+        public int TotalRoles { get; set; }
+        public int TotalLocations { get; set; }
+        public int TotalGenres { get; set; }
+        public int TotalUsers { get; set; }
+    }
+
+}
---
+++ BookShopApplication.Web/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using BookShopApplication.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopApplication.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        private readonly IAdminService _adminService;
+
+        public AdminController(IAdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await _adminService.GetAdminDashboardData();
+            return View(model);
+        }
+    }
+}
