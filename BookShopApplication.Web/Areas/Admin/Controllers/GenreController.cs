58584d75d49e01ad4113c0cd02865428fab42358
Stella Uzunova <stellauznv@gmail.com>
2025-07-29 13:56:23 +0300

    Admin Genre index is now available.

---
+++ BookShopApplication.Web/Areas/Admin/Controllers/GenreController.cs
@@ -0,0 +1,24 @@
+﻿using BookShopApplication.Services.Contracts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookShopApplication.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class GenreController : Controller
+    {
+        private readonly IGenreService _genreService;
+
+        public GenreController(IGenreService genreService)
+        {
+            _genreService = genreService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var genres = await _genreService.GetGenreListAsync();
+            return View(genres);
+        }
+    }
+}
