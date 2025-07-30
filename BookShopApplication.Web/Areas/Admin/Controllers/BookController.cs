57226e246bd5e490736b240864a827db73396ecf
Stella Uzunova <stellauznv@gmail.com>
2025-07-30 13:06:25 +0300

    Books can now be created from the shop details or index in the admin dashboard.

--- BookShopApplication.Web/Areas/Admin/Controllers/BookController.cs
+++ BookShopApplication.Web/Areas/Admin/Controllers/BookController.cs
@@ -1,9 +1,12 @@
 ﻿using BookShopApplication.Data.Models;
 using BookShopApplication.Data.Repository.Contracts;
+using BookShopApplication.Services;
 using BookShopApplication.Services.Contracts;
-using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
+using BookShopApplication.Web.ViewModels.Book;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Security.Claims;
 
 namespace BookShopApplication.Web.Areas.Admin.Controllers
 {
@@ -12,16 +15,55 @@ namespace BookShopApplication.Web.Areas.Admin.Controllers
     public class BookController : Controller
     {
         private readonly IBookService _bookService;
+        private readonly IGenreService _genreService;
 
-        public BookController(IBookService bookService)
+        public BookController(IBookService bookService, IGenreService genreService)
         {
             _bookService = bookService;
+            _genreService = genreService;
         }
 
         public async Task<IActionResult> Index()
         {
             var models = await _bookService.DisplayAllBooksAsync();
             return View(models);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Create(Guid shopId)
+        {
+            var genresModel = await _genreService.GetGenreListAsync();
+
+            var model = new CreateBookViewModel
+            {
+                Genres = genresModel.Select(g => new SelectListItem
+                {
+                    Text = g.Name,
+                    Value = g.Id.ToString()
+                }).ToList(),
+                ShopId = shopId
+            };
+            return View(model);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateBookViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                // Reload genres in case of validation failure
+                var genresModel = await _genreService.GetGenreListAsync();
+                model.Genres = genresModel.Select(g => new SelectListItem
+                {
+                    Text = g.Name,
+                    Value = g.Id.ToString()
+                }).ToList();
+
+                return View(model);
+            }
+
+            await _bookService.CreateBookAsync(model);
+
+            return RedirectToAction("Details", "Shop", new { id = model.ShopId });
         }
     }
 }
